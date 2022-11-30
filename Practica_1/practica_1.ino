d7a21ea0a89390968c6b0989efab2d8dd214f221
Emilia Zuñiga Losada <82168337+Emilia-Zuniga@users.noreply.github.com>
2022-11-30 11:57:00 -0600

    Encender un Led

---
+++ Practica_1/practica_1.ino
@@ -0,0 +1,21 @@
+
+int LED_PIN = 2; //Declarar el PIN
+// Ejecuta una sola vez las siguientes instrucciones
+void setup() {
+   // El led se conecta a un pin de salida
+   pinMode(LED_PIN, OUTPUT);
+}
+// Ciclo que repite las siguientes instrucciones
+void loop() {
+   // Enciende el LED (a nivel alto)
+   digitalWrite(LED_PIN, HIGH);
+
+   // Espera 1000 milisegundos (1 segundo)
+   delay(1000);
+
+   // Apaga el pin 2 (a nivel bajo)
+   digitalWrite(LED_PIN, LOW);
+
+   // Espera 1000 milisegundos (1 segundo)
+   delay(1000);
+}
